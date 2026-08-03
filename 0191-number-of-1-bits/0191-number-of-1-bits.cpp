874f8a3d04d9c5061efbd454e206fb4b8c2372cb
Mrigank Singh <mriganksingh51@gmail.com>
2026-08-03 23:11:19 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.22%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     int hammingWeight(int n) {
      int count=0;
-     int x;
-     while(n!=0){
-        n=n&n-1;
-        count++;
-        
-     } 
+     count=__builtin_popcount(n);
      return count;
     }
 };
